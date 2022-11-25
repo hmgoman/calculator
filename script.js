13e681ee875f737e1385cc33fbbb3c0fc2164d29
Helen Goman <hmgoman@gmail.com>
2022-11-25 11:32:14 -0500

    .

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const number = document.querySelectorAll(".number");
+const operation = document.querySelectorAll(".operator");
+const full_display = document.getElementById("dc1");
+const current_display = document.getElementById("dc2");
+const equal = document.getElementById("equal");
+const clear = document.getElementById("clear");
+const del = document.getElementById("delete");
+const decimal = document.getElementById("decimal");
+var firstOp = "";
+var operator = "";
+var secondOp = "";
+var solution = "";
+var count = 0;
+
 function add(a, b) {
     return a + b;
 }
@@ -11,39 +25,79 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if(b == 0) { return "Error"}
     return a / b;
 }
 
 function operate(operator, a, b) {
-    if (operator == add) {
-        return add(a, b);
+    if (operator == "+") {
+        solution = add(a, b);
     }
-    else if (operator == subtract) {
-        return subtract(a, b);
+    else if (operator == "-") {
+        solution = subtract(a, b);
     }
-    else if (operator == multiply) {
-        return multiply(a, b);
+    else if (operator == "x") {
+        solution = multiply(a, b);
     }
-    else if (operator == divide) {
-        return divide(a, b);
+    else if (operator == "÷") {
+        solution = divide(a, b);
+    }
+    full_display.innerHTML = firstOp + " " + operator + " " + secondOp + " =";
+    current_display.innerHTML = Math.round(solution * 1000)/1000;
+}
+
+number.forEach(number => {
+    number.addEventListener("click", (e) => {
+        if(isNaN(current_display.textContent) == true) {
+            current_display.innerHTML = e.target.textContent;
+        }
+        else if(isNaN(current_display.textContent) == false) {
+            current_display.innerHTML = current_display.textContent + e.target.textContent;
+        }
+        if(operator == "") {
+            firstOp = Number(current_display.textContent);
+        }
+        secondOp = Number(current_display.textContent);
+    });
+})
+
+operation.forEach(operation => {
+    operation.addEventListener("click", (e) => {
+        evaluate();
+        full_display.innerHTML = firstOp + " " + e.target.textContent;
+        current_display.innerHTML = e.target.textContent;
+        operator = e.target.textContent;
+    })
+})
+
+function evaluate() {
+    if(current_display.textContent == solution) {
+        firstOp = solution; 
     }
 }
 
-const number = document.querySelectorAll(".number");
-const operator = document.querySelectorAll(".operator");
-const current_display = document.getElementById("dc1");
+function clearScreen() {
+    current_display.innerHTML = "";
+    full_display.innerHTML = "";
+    firstOp = "";
+    operator = "";
+    secondOp = "";
+}
 
-function displayNum(x) {
-    for(var i = 0; i < number.length; i++) {
-        current_display.innerHTML = number[x].textContent;
+function delCharacter() {
+    current_display.innerHTML = current_display.innerHTML.slice(0, (current_display.innerHTML.length-1));
+}
+
+function addDecimal() {
+    if(current_display.textContent === "" || current_display.textContent === operator) {
+        current_display.innerHTML = "0.";
+    }
+    if(current_display.textContent.includes(".") === false) {
+        current_display.innerHTML = current_display.innerHTML + ".";
     }
 }
 
-function displayOp(y) {
-    for(var i = 0; i < operator.length; i++) {
-        current_display.innerHTML = operator[y].textContent;
-    }
-}
-
-
-
+equal.addEventListener("click", () => operate(operator, firstOp, secondOp));
+clear.addEventListener("click", clearScreen);
+del.addEventListener("click", delCharacter);
+decimal.addEventListener("click", addDecimal);
